816167eaec6630d9719aaa06413ddc8d8f63953f
cherrykooki <97518721+cherrykooki@users.noreply.github.com>
2022-01-14 00:28:14 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+#Initialize array     
+arr = [7, 2, 4, 9, 1];     
+temp = 0;    
+     
+#Displaying elements of original array    
+print("Elements of original array: ");    
+for i in range(0, len(arr)):    
+    print(arr[i], end=" ");    
+     
+#Sort the array in ascending order    
+for i in range(0, len(arr)):    
+    for j in range(i+1, len(arr)):    
+        if(arr[i] > arr[j]):    
+            temp = arr[i];    
+            arr[i] = arr[j];    
+            arr[j] = temp;    
+     
+print();    
+     
+#Displaying elements of the array after sorting    
+    
+print("Elements of array sorted in ascending order: ");    
+for i in range(0, len(arr)):    
+    print(arr[i], end=" ");   
+
+
+
+
+#Sort the array in descending order    
+for i in range(0, len(arr)):    
+    for j in range(i+1, len(arr)):    
+        if(arr[i] < arr[j]):    
+            temp = arr[i];    
+            arr[i] = arr[j];    
+            arr[j] = temp;    
+     
+print();    
+     
+#Displaying elements of array after sorting    
+print("Elements of array sorted in descending order: ");    
+for i in range(0, len(arr)):     
+    print(arr[i]),    
